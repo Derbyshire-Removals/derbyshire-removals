37be26374b213ad89bf39f8945e9dfeae415eb9c
Kamalpreet <kmlprtsng@gmail.com>
2024-09-07 15:22:00 +0100

    add derbyshire trusted trader link

--- src/components/Authority.jsx
+++ src/components/Authority.jsx
@@ -6,11 +6,13 @@ const Authority = () => {
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-10">Derbyshire's Trusted Trader</h2>
         <div className="flex justify-center items-center">
-          <img 
-            src="images/derbyshire-trusted-trader-logo.gif" 
-            alt="Derbyshire's Trusted Trader Logo" 
-            className="h-32 w-auto"
-          />
+          <a href="https://apps.derbyshire.gov.uk/applications/trusted-trader/profile.asp?tid=56236">
+            <img
+              src="images/derbyshire-trusted-trader-logo.gif" 
+              alt="Derbyshire's Trusted Trader Logo" 
+              className="h-32 w-auto"
+            />
+          </a>
         </div>
       </div>
     </section>
