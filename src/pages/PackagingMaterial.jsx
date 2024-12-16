4d435d2daddd1729d83b386eb8aeb58823d70c5f
Kamalpreet <kmlprtsng@gmail.com>
2024-12-16 14:09:38 +0000

    update website prices

--- src/pages/PackagingMaterial.jsx
+++ src/pages/PackagingMaterial.jsx
@@ -11,29 +11,14 @@ import {
 } from "@/components/ui/table"
 
 const packagingMaterials = [
-  { description: "Medium/short (book box)", dimensions: "18x13x13        BOOK BOX", price: "£3.30ea" },
-  { description: "Large box", dimensions: "18x18x20        GLASS/CHINA LINEN ETC", price: "£3.80ea" },
-  { description: "Extra large box", dimensions: "18x18x30", price: "£4.80" },
-  { description: "Linen box", dimensions: "18x36x10        LINEN CLOTHES ETC", price: "£4.80" },
-  { description: "Cd/dvd box", dimensions: "", price: "£3.20" },
-  { description: "Wine box", dimensions: "", price: "£4.45" },
-  { description: "Movers Pack", dimensions: "10 x lg box,10 x sm box,10 metres bubble wrap,2.5 kg paper,5 x blue bags,1 x cd/dvd box,1 x blitz box,2 x tape,1 x fragile tape,1x marker pen,2 x seat covers,1 x sofa cover,1 x 6ft mattress cover, 1 x re usable bag.", price: "£85.00" },
-  { description: "Wardrobe Cartons", dimensions: "", price: "£8.99" },
-  { description: "(Rental)", dimensions: "", price: "£5.00" },
-  { description: "Tape (brown)", dimensions: "", price: "£2" },
-  { description: "Tape (fragile)", dimensions: "", price: "£2" },
-  { description: "Tape gun", dimensions: "", price: "£6.00" },
-  { description: "Paper Ream", dimensions: "", price: "£13.50" },
-  { description: "Tissue Paper per ream", dimensions: "", price: "£9.50" },
-  { description: "Bubble wrap", dimensions: "750mmx25mtrs   ,     also available 100/200 metre rolls", price: "£15.45" },
-  { description: "Cushion Wrap per metre", dimensions: "", price: "£2.50" },
-  { description: "Webbing per roll", dimensions: "", price: "£10.00" },
-  { description: "Blankets", dimensions: "", price: "£4.00" },
-  { description: "Sofa Cover", dimensions: "", price: "£3.50" },
-  { description: "Chair Cover x2", dimensions: "", price: "£3.50" },
-  { description: "king size mattress cover", dimensions: "Plastic", price: "£4.95" },
-  { description: "Double mattress cover", dimensions: "Plastic", price: "£4.95" },
-  { description: "Single mattress cover", dimensions: "Plastic", price: "£3.95" },
+  { description: "Medium box", dimensions: "18x13x13 inch - Book Box", price: "£3.30" },
+  { description: "Large box", dimensions: "18x18x20 inch - Glass/China Linen etc", price: "£4.20" },
+  { description: "Wardrobe Cartons (Rental)", dimensions: "", price: "£7.50" },
+  { description: "Tape (brown)", dimensions: "", price: "£1.60 per roll" },
+  { description: "Tape (fragile)", dimensions: "", price: "£1.60 per roll" },
+  { description: "Thick White Paper", dimensions: "20 x 30 inch - 5kg", price: "£15.95" },
+  { description: "Bubble wrap", dimensions: "750mm x 25meters - Green BIO", price: "£16.95" },
+  { description: "Black Marker Pen", dimensions: "", price: "£1.75" },
 ];
 
 const PackagingMaterial = () => {
@@ -51,7 +36,7 @@ const PackagingMaterial = () => {
             <TableRow>
               <TableHead>Description</TableHead>
               <TableHead>Dimensions</TableHead>
-              <TableHead>Price</TableHead>
+              <TableHead>Price per item</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
